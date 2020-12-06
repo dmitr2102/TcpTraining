a29e172315d5403ed62108aa56520e328f803b48
dmitr2102 <dmitr2102@gmail.com>
2020-12-06 14:16:30 +0300

    Add Start Void

--- Listener/Listener.cs
+++ Listener/Listener.cs
@@ -1,9 +1,21 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace ListenerNamespace
 {
     public class Listener
     {
         public void Start(){
-            
+            TcpListener server = null;
+            // Set the TcpListener on port 13000.
+            int port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
         }
     }
 }
